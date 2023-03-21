85f100ffafa3fcf79734e4b2e7dcaf881ec98dfb
srinivas <rao.srinivas3@gmail.com>
2023-03-22 03:26:46 +0530

    Learn Terraform

--- Learn_Terraform/Sample-1/test.tf
+++ Learn_Terraform/Sample-1/test.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ec2" {
-  ami = "ami-0a017d8ceb274537d"
+  ami = "ami-0089b8e98cd95257d"
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-0a6d1d54d9f7230a1"]
   tags = {
