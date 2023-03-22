ac0ebfff8117cc2b2f82d0a6fbe89c8b49f90b4f
srinivas <rao.srinivas3@gmail.com>
2023-03-23 03:59:10 +0530

    Learn Terraform

--- Learn_Terraform/Sample-1/var11.tf
+++ Learn_Terraform/Sample-1/var11.tf
@@ -7,3 +7,21 @@ variable "sam2" {
 variable "sam3" {
   default = "sg-0a6d1d54d9f7230a1"
 }
+variable "sample11" {
+  default = "Hello"
+}
+
+variable "sample22" {
+  default = ["Hello",100,true,"world"]
+}
+
+variable "sample33" {
+  default = {
+    string = "Hell",
+    number = 200,
+    boolean = true
+  }
+}
+output "types" {
+  value = "Variable sample11-${var.sample11},First value in list-${var.sample22[0]},map value-${var.sample33[boolean]}"
+}
