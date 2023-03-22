ad40b6be6c089fe48157ffd8def3cd0fd7c78b98
srinivas <rao.srinivas3@gmail.com>
2023-03-22 06:08:25 +0530

    Learn Terraform

--- Learn_Terraform/Output/out.tf
+++ Learn_Terraform/Output/out.tf
@@ -3,15 +3,15 @@ output "sample" {
 }
 
 output "instance_ip_address" {
- value = aws_instance.rao11.public_ip
+ value = aws_instance.rao123.ebs_block_device
 }
 
-resource "aws_instance" "rao11" {
+resource "aws_instance" "rao123" {
   ami = "ami-0089b8e98cd95257d"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-0a6d1d54d9f7230a1"]
   tags = {
-    Name = "sri"
+    Name = "terra"
 
   }
 
