04c24cea80c4d188581c67efe85e5e414773893a
srinivas <rao.srinivas3@gmail.com>
2023-03-22 06:17:17 +0530

    Learn Terraform

--- Learn_Terraform/Output/out.tf
+++ Learn_Terraform/Output/out.tf
@@ -6,6 +6,10 @@ output "instance_ip_address" {
  value = aws_instance.rao123.availability_zone
 }
 
+output "getting_vlumes" {
+  value = aws_instance.rao123.host_id
+}
+
 resource "aws_instance" "rao123" {
   ami = "ami-0089b8e98cd95257d"
   instance_type = "t2.micro"
